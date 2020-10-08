d448c892b7e461b412758cd086e2c8cd42c74bca
sugimoto <sugimoto@batonz.co.jp>
2020-10-08 15:52:51 +0900

    実装ミスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  # get 'tasks/index', to: 'tasks#index'
-  resources :tasks, only: %i(index create show new edit update)
+  resources :tasks, only: %i(index create show new edit update delete)
 end
