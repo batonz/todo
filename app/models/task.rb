a88c8d1a08bcfaf5a45b0b22f59c6e3b2eba2260
sugimoto <sugimoto@batonz.co.jp>
2020-10-08 16:24:20 +0900

    i18n用のファイルを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 3 }
+  validates :title, presence: true, length: { maximum: 3, too_long: "が長すぎます" }
   validates :description, presence: true
 end
