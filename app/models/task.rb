1d46e58d01a7872bab69dbd28ff60de6559e1c32
sugimoto <sugimoto@batonz.co.jp>
2020-10-08 15:57:48 +0900

    タイトルを３文字以上入力できない様。

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 30 }
+  validates :title, presence: true, length: { maximum: 3 }
   validates :description, presence: true
 end
